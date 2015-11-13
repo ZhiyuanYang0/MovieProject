d5493829ab735a3bc27f8df42eb7df39f2ca0afe
samuelyang <zhiyuanyang0@gmail.com>
2015-11-13 03:09:53 -0800

    add actors and daos

---
+++ MovieDotCom/src/movie/com/model/FavoriteActors.java
@@ -0,0 +1,32 @@
+package movie.com.model;
+
+public class FavoriteActors {
+	protected int favoriteActorId;
+	protected Users user;
+	protected Actors actor;
+	public FavoriteActors(int favoriteActorId, Users user, Actors actor) {
+		super();
+		this.favoriteActorId = favoriteActorId;
+		this.user = user;
+		this.actor = actor;
+	}
+	public int getFavoriteActorId() {
+		return favoriteActorId;
+	}
+	public void setFavoriteActorId(int favoriteActorId) {
+		this.favoriteActorId = favoriteActorId;
+	}
+	public Users getUser() {
+		return user;
+	}
+	public void setUser(Users user) {
+		this.user = user;
+	}
+	public Actors getActor() {
+		return actor;
+	}
+	public void setActor(Actors actor) {
+		this.actor = actor;
+	}
+
+}
